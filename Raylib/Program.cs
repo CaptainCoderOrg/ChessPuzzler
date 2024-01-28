8eddaf742c958a9c6fcea2fb94750931eba5e923
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-01-27 19:18:23 -0700

    chore: Initialize Raylib.

---
+++ Raylib/Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+
+namespace HelloWorld;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
